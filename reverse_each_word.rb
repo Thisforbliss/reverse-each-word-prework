8592f1e7c07284c739724b76fc1cc22c1d2fca53
Malcolm Leake <echelon1089@gmail.com>
2018-08-16 21:20:18 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,7 @@ sentence1 = "Tell me whats going on right now. "
 
 def reverse_each_word(sentence1)
   sentence1.split.collect do |sentence|
-   sentence.reverse.join
+   sentence.reverse
    end
 end
 
